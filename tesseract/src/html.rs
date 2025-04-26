5b155f8808972860da115afa43d3edf5d2c0f859
Eyrim <marfxedbot@gmail.com>
2025-04-27 00:45:17 +0100

    Write macros to generate html

--- tesseract/src/html.rs
+++ tesseract/src/html.rs
@@ -1,18 +1,64 @@
-extern crate html_element;
+macro_rules! properties {
+    ($key:ident = $value:expr) => {
+        format!("{}={} ", $key, $value)
+    };
+    ($($key:expr, $value:expr);+) => {
+        {
+            let mut v: Vec<String> = Vec::new();
 
-pub trait HTMLElement {
-    fn render() -> String;
+            $(
+                v.push(format!("{} = {}", $key, quotes!($value)));
+            )+
+
+           v.join(" ")
+        }
+    };
 }
 
-#[derive(html_element::HTMLElement)]
+macro_rules! quotes {
+    ($value:expr) => {
+        format!(r#""{}""#, $value)
+    };
+}
+
+macro_rules! tag {
+    ($tag:expr, $properties:expr, $body:expr) => {
+        format!("<{} {}>\n{}\n<{}/>", $tag, $properties, $body, $tag)
+    };
+}
+
+pub trait HTMLElement {
+    fn render(&self) -> String;
+}
+
 pub struct Head<'a> {
     pub title: &'a str,
     pub lang: &'a str,
 }
 
 impl HTMLElement for Head<'_> {
-    fn render() -> String {
-        todo!()
+    fn render(&self) -> String {
+        tag!(
+            "head",
+            properties!("title", self.title; "lang", self.lang),
+            ""
+        )
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_render_head_tag() {
+        let head = Head {
+            title: "test tag",
+            lang: "en-gb",
+        };
+
+        let expected = "<head title = \"test tag\" lang = \"en-gb\">\n\n<head/>";
+
+        assert_eq!(head.render(), expected);
+    }
+}
